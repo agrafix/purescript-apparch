6d4576a7107c9ddaa1a484bf58ea0834d3fa8510
Alexander Thiemann <mail@athiemann.net>
2016-01-01 20:43:57 +0100

    allow hooking of effects

--- src/Main.purs
+++ src/Main.purs
@@ -2,6 +2,7 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff
+import Control.Monad.Eff.Console
 
 import Data.Maybe.Unsafe (fromJust)
 import Data.Nullable
@@ -28,35 +29,61 @@ type CompProps m a =
   , address :: Channel (Array a)
   }
 
-type Config m a =
-  { init :: m
-  , update :: a -> m -> m
+type EffModel eff m a =
+  { model :: m
+  , effects :: Effects eff a
+  }
+
+type Effects eff a = Array (Eff (dom :: DOM, chan :: Chan | eff) a)
+
+noFx :: forall eff m a. m -> EffModel eff m a
+noFx m = { model: m, effects: noEffects }
+
+noEffects :: forall eff a. Effects eff a
+noEffects = []
+
+type Config eff m a =
+  { init :: EffModel eff m a
+  , update :: a -> m -> EffModel eff m a
   , view :: ReactClass (CompProps m a)
   , inputs :: Array (S.Signal a)
   , renderTarget :: ElementId
   }
 
-runApp :: forall m a e. Config m a -> Eff (dom :: DOM, chan :: Chan | e) Unit
+runApp :: forall eff m a. Config eff m a -> Eff (dom :: DOM, chan :: Chan | eff) Unit
 runApp config = do
   actionChan <- channel []
   let actionSignal = subscribe actionChan
       inputs =
         fromJust $
         S.mergeMany (actionSignal : map (map singleton) config.inputs)
-      modelSignal =
-        S.foldp (\actions m -> foldl (flip config.update) m actions) config.init inputs
+      updateStep action effModel =
+        let newEffModel = config.update action effModel.model
+        in { model: newEffModel.model
+           , effects: effModel.effects ++ newEffModel.effects
+           }
+      update actions effModel =
+        foldl (flip updateStep) (noFx effModel.model) actions
+      effModelSignal =
+        S.foldp update config.init inputs
   tgt <- findRenderTarget config.renderTarget
-  let renderModel m = do
-         let props =
+  let renderRunModel effModel = do
+         let wiredEffects =
+                 flip map effModel.effects $ \runEff -> do
+                 res <- runEff
+                 send actionChan [res]
+             m = effModel.model
+             props =
                { model: m
                , address: actionChan
                }
              factory =
                createFactory config.view props
+         sequence_ wiredEffects
          _ <- render factory tgt
          return unit
       renderSignal =
-        map renderModel modelSignal
+        map renderRunModel effModelSignal
   S.runSignal renderSignal
 
 findRenderTarget :: forall e. ElementId -> Eff (dom :: DOM | e) Element
@@ -75,15 +102,23 @@ type Model = { counter :: Int }
 data Action
   = Increment
   | Decrement
+  | Nop
 
 initModel :: Model
 initModel = { counter: 0 }
 
-update :: Action -> Model -> Model
+update :: forall eff. Action -> Model -> EffModel (console :: CONSOLE | eff) Model Action
 update act m =
   case act of
-    Increment -> m { counter = m.counter + 1 }
-    Decrement -> m { counter = m.counter - 1 }
+    Nop -> noFx m
+    Increment ->
+      { model: m { counter = m.counter + 1 }
+      , effects:
+          [ do log "Increment"
+               return Nop
+          ]
+      }
+    Decrement -> noFx $ m { counter = m.counter - 1 }
 
 view :: ReactClass (CompProps Model Action)
 view = createClass $ spec unit \ctx -> do
@@ -103,7 +138,7 @@ view = createClass $ spec unit \ctx -> do
 
 main =
   runApp
-   { init: initModel
+   { init: noFx initModel
    , update: update
    , view: view
    , inputs: []
